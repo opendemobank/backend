59dfa5ca3d91cc156737d9dcfd749d30209c3f87
Toomas Tamm <toomas@toomastamm.ee>
2021-11-26 12:23:25 +0200

    Add bearer auth to swagger ui

--- src/main/java/com/opendemobank/backend/BackendApplication.java
+++ src/main/java/com/opendemobank/backend/BackendApplication.java
@@ -1,7 +1,14 @@
 package com.opendemobank.backend;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +17,22 @@ public class BackendApplication {
         SpringApplication.run(BackendApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        final String apiTitle = String.format("%s API", StringUtils.capitalize("OpenDemoBank"));
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                )
+                )
+                .info(new Info().title(apiTitle));
+    }
 }
+
