18f3c9c47dcce6cb62dc98b086a18a14d1e58ca4
Karoliine Holter <karoliine.holter@ut.ee>
2021-12-08 16:13:02 +0200

    Review remarks done

--- src/main/java/com/opendemobank/backend/controller/TransactionController.java
+++ src/main/java/com/opendemobank/backend/controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.opendemobank.backend.controller;
 import com.opendemobank.backend.domain.*;
 import com.opendemobank.backend.repository.TransactionsRepo;
 import com.opendemobank.backend.repository.UsersRepo;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -24,7 +25,7 @@ public class TransactionController {
     UsersRepo usersRepo;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Transaction> getById(@AuthenticationPrincipal User currentUser, @PathVariable("id") long id) {
+    public ResponseEntity<Transaction> getById(@Parameter(hidden = true) @AuthenticationPrincipal User currentUser, @PathVariable("id") long id) {
 
         // find transaction by id
         Transaction transaction = transactionsRepo.findById(id);
@@ -41,19 +42,19 @@ public class TransactionController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Transaction>> getAll(@AuthenticationPrincipal User currentUser) {
+    public ResponseEntity<List<Transaction>> getAll(@Parameter(hidden = true) @AuthenticationPrincipal User currentUser) {
         if (!currentUser.getRole().equals(Role.ADMIN)) return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 
         return new ResponseEntity<>(transactionsRepo.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/user/{id}")
-    public ResponseEntity<List<Transaction>> getAllBySessionUserId(@AuthenticationPrincipal User currentUser, @PathVariable("id") long id) {
+    public ResponseEntity<List<Transaction>> getAllBySessionUserId(@Parameter(hidden = true) @AuthenticationPrincipal User currentUser, @PathVariable("id") long id) {
         User user = usersRepo.findById(id);
         if (user == null)
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
-        if (currentUser.getId() != id)
+        if (currentUser.getId() != id && currentUser.getRole() != Role.ADMIN)
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 
         List<Transaction> transactions = transactionsRepo.findAllBySessionUserId(id);
@@ -62,9 +63,10 @@ public class TransactionController {
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Transaction> createTransaction(@AuthenticationPrincipal User currentUser, @RequestBody Transaction newTransaction) {
+    public ResponseEntity<Transaction> createTransaction(@Parameter(hidden = true) @AuthenticationPrincipal User currentUser, @RequestBody Transaction newTransaction) {
         // if the session user is not the same as the one that is stated to have created the transaction
-        if (!Objects.equals(currentUser.getId(), newTransaction.getSessionUser().getId()))
+        // or if the session user is not admin
+        if (!Objects.equals(currentUser.getId(), newTransaction.getSessionUser().getId()) && currentUser.getRole() != Role.ADMIN)
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
 
         return new ResponseEntity<>(transactionsRepo.save(newTransaction), HttpStatus.CREATED);
