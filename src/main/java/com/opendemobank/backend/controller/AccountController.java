4d399c60de3e0a6e7ee4220bd9b8d1be3487447f
lukasbaltramaitis <43213136+lukasbaltramaitis@users.noreply.github.com>
2021-12-14 01:26:43 +0200

    Update AccountController.java

--- src/main/java/com/opendemobank/backend/controller/AccountController.java
+++ src/main/java/com/opendemobank/backend/controller/AccountController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.util.List;
-
+ 
 @RestController
 @RequestMapping("/api/accounts")
 public class AccountController {
